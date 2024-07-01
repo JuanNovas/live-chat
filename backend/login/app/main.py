b0733c01b2efa1c5db5b2445e8e71ddb5bb326ce
Juan Novas <juanignacionovas@gmail.com>
2024-07-01 17:57:45 -0300

    route update

--- backend/login/app/main.py
+++ backend/login/app/main.py
@@ -3,5 +3,5 @@ from api.endpoints import register, login
 
 app = FastAPI()
 
-app.include_router(register.register_router, prefix="/register")
-app.include_router(login.login_router, prefix="/login")
+app.include_router(register.register_router, prefix="")
+app.include_router(login.login_router, prefix="")
