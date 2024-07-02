e3d2a9bb3a46a25ecab4434e8a71d38253564b05
Juan Novas <juanignacionovas@gmail.com>
2024-07-02 16:02:11 -0300

    endpoint now use tokenschema

--- backend/verification/app/api/endpoints/validate_token.py
+++ backend/verification/app/api/endpoints/validate_token.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from schema.token import TokenSchema
 from core.security import verify_token
 
 get_user_router = APIRouter()
 
 @get_user_router.post("/validate_token")
-async def validate_token(token: str):
-    user = verify_token(token)
+async def validate_token(token: TokenSchema):
+    user = verify_token(token.token)
     return {"user": user}
